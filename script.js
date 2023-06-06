774dd6d23ca27dff7429b9b9dd699cc7edf77167
portgee <giolgarcia05@gmail.com>
2023-06-06 12:54:41 -0400

    update

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ function toggleTheme() {
   const sun = document.querySelector(".sun");
   const moon = document.querySelector(".moon");
   const body = document.querySelector("body");
+  const about = document.getElementById("aboutMe")
 
   moon.classList.toggle("hidden");
   sun.classList.toggle("hidden");
+  about.style.backgroundColor = moon.classList.contains("hidden")
+    ? "#947BEC"
+    : "black"
+
   body.style.backgroundImage = moon.classList.contains("hidden")
     ? 'url("assets/light-background.gif")'
     : 'url("assets/dark-background.gif")';
